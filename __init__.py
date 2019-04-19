0204f7f53491f1e6bc3a6449cad02a5e541c5166
Kris Gesling <krisgesling@gmail.com>
2019-04-19 10:13:11 +0930

    fix code formatting

--- __init__.py
+++ __init__.py
@@ -8,13 +8,12 @@ class RoomsLocation(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_handler(IntentBuilder("").require("querry").require("Room")
+    @intent_handler(IntentBuilder("").require("querry").require("Room"))
     def handle_departments_focus(self, message):
         if message.data["Room"] == "bathroom":
             self.speak_dialog('bathroom.location')
-	elif message.data["Room"] == "bedroom":
-	    self.speak_dialog('Bedroom.location')
+        elif message.data["Room"] == "bedroom":
+            self.speak_dialog('Bedroom.location')
 
 def create_skill():
     return RoomsLocation()
-
